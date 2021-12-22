3c38bf800c24681b821a33ab4207cfc51fc66021
Totto16 <tobiausgais@gmail.com>
2021-12-22 17:34:21 +0100

    Day 22 (part 2) complete

--- Day 22/index.js
+++ Day 22/index.js
@@ -52,49 +52,72 @@ function solve(input, mute = false) {
         } else {
             cube.forEach((point) => UniquePoints.delete(point.join(',')));
         }
-        if (!mute) {
-            console.log(`${i + 1} / ${parsed.length}`);
-        }
     }
     return UniquePoints.size;
 }
 
 function solve2(input, mute = false) {
-    let parsed = input
-        .map((inp) => {
-            let [_, state, ...rest] = inp.match(
-                /(\w*)\sx=(-?\d*)\.\.(-?\d*),y=(-?\d*)\.\.(-?\d*),z=(-?\d*)\.\.(-?\d*)/i
-            );
-            let [x1, x2, y1, y2, z1, z2] = rest.map((a) => parseInt(a));
-            return {
-                state: state == 'on',
-                x: [x1, '..', x2].fillElements(),
-                y: [y1, '..', y2].fillElements(),
-                z: [z1, '..', z2].fillElements(),
-            };
-        })
-        .filter((a) => a != null);
+    let parsed = input.map((inp) => {
+        let [_, state, ...rest] = inp.match(/(\w*)\sx=(-?\d*)\.\.(-?\d*),y=(-?\d*)\.\.(-?\d*),z=(-?\d*)\.\.(-?\d*)/i);
+        let [x1, x2, y1, y2, z1, z2] = rest.map((a) => parseInt(a));
+        return {
+            state: state == 'on',
+            coords: [
+                [x1, x2],
+                [y1, y2],
+                [z1, z2],
+            ],
+        };
+    });
 
-    let UniquePoints = new Set();
-    for (let i = 0; i < parsed.length; i++) {
-        let current = parsed[i];
-        let cube = current.x.combine(current.y).combine(current.z);
-        if (current.state) {
-            cube.forEach((point) => UniquePoints.add(point.join(',')));
-        } else {
-            cube.forEach((point) => UniquePoints.delete(point.join(',')));
-        }
-        if (!mute) {
-            console.log(`${i + 1} / ${parsed.length}`);
-        }
-    }
-    return UniquePoints.size;
+    const OnCoords = parsed
+        .reduce((acc, line) => {
+            let mapFunction = (coords) => {
+                let intersect = coords
+                    .map((limit, i) => [Math.max(line.coords[i][0], limit[0]), Math.min(line.coords[i][1], limit[1])])
+                    .filter(([one, two]) => two >= one);
+                if (intersect.length != 3) {
+                    return [coords];
+                } else {
+                    let splitFunction = (cube, tmp, count = 0) => {
+                        let firstPart = [
+                            [cube[count][0], tmp[count][0] - 1],
+                            [tmp[count][1] + 1, cube[count][1]],
+                        ]
+                            .filter(([b1, b2]) => b2 >= b1)
+                            .map((limit) => Object.assign([...cube], { [count]: limit }));
+
+                        let secondPart = [];
+                        if (count < 2) {
+                            secondPart = splitFunction(
+                                Object.assign([...cube], { [count]: tmp[count] }),
+                                tmp,
+                                count + 1
+                            );
+                        }
+
+                        return [...firstPart, ...secondPart];
+                    };
+
+                    return splitFunction(coords, intersect);
+                }
+            };
+
+            let rest = line.state ? [line.coords] : [];
+            let result = [...acc.flatMap(mapFunction), ...rest];
+            return result;
+        }, [])
+        .map((a) => a.reduce((acc, [first, second]) => acc * (second - first + 1), 1))
+        .reduce((acc, cnt) => cnt + acc, 0);
+
+    return OnCoords;
 }
 
 function testAll() {
     let t_input = [getFile('./sample.txt'), getFile('./sample2.txt')];
+    let t_input2 = [getFile('./sample3.txt')];
     let t_result = [39, 590784];
-    let t_result2 = [39, 2758514936282235];
+    let t_result2 = [2758514936282235];
 
     for (let i = 0; i < t_input.length; i++) {
         const testInput = t_input[i];
@@ -106,8 +129,11 @@ function testAll() {
             );
             process.exit(69);
         }
+    }
+    for (let i = 0; i < t_input2.length; i++) {
+        const testInput2 = t_input2[i];
         const testResult2 = t_result2[i];
-        let test2 = solve2(testInput, true);
+        let test2 = solve2(testInput2, true);
         if (test2 != testResult2) {
             console.error(
                 `Wrong Solving Mechanism on Iteration ${i + 1} Test 2: Got '${test2}' but expected '${testResult2}'`
@@ -235,6 +261,18 @@ function initPrototype() {
             return newArray;
         },
     });
+
+    Object.defineProperty(Array.prototype, 'print', {
+        value: function () {
+            try {
+                let toPrint = JSON.stringify(this);
+                console.log(toPrint);
+            } catch (e) {
+                return false;
+            }
+            return;
+        },
+    });
 }
 
 function slowWarning() {
@@ -264,7 +302,7 @@ async function main() {
     if (doTests) {
         testAll();
     }
-    // This solution is also not that slow, but for that amount of parallel universes it takes over 2 secs
+    // This solution is slow for the first approach, but i wanted to use my array functions fillElements (in other langauges [x..y] just works :()) and combine, I coded combine but never actually used it, but with that it is a super readable solution :)
     if (autoSkipSlow) {
         console.log('Auto Skipped Moderately Slow');
         process.exit(43);
